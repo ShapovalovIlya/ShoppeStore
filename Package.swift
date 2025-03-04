de5b8fc2f1853477eb8af9ae53f4d89a57a74de3
Shapovalov.i <isdaiquiri@gmail.com>
2025-03-04 19:09:57 +0300

    Change swift-tools-version 6.0 -> 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -25,7 +25,7 @@ let package = Package(
                 .product(name: "SwiftFP", package: "SwiftFP")
             ],
             swiftSettings: [
-//                .enableUpcomingFeature("StrictConcurrency")
+                .enableUpcomingFeature("StrictConcurrency")
             ]
         ),
         .testTarget(
